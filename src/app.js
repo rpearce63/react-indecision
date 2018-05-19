4aeadcac1ba3746617a416c44895f2990f3a70b5
Rick Pearce <rpearce63@yahoo.com>
2018-05-19 07:37:00 -0500

    use state to update options

--- src/app.js
+++ src/app.js
@@ -3,14 +3,28 @@ class IndecisionApp extends React.Component {
     super(props);
     this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
     this.handlePick = this.handlePick.bind(this);
+    this.handleAddOption = this.handleAddOption.bind(this);
     this.state = {
-      options: ['Thing one', 'Thing two', 'Thing four']
+      options: []
     }
   }
   handleDeleteOptions() {
     this.setState(() => {
       return {
         options: []
+      };
+    });
+  }
+
+  handleAddOption(option) {
+    if(!option) {
+      return 'Enter valid value to add item';
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'This option already exists.'
+    } 
+    this.setState((prevState) => {
+      return {
+        options:prevState.options.concat(option)
       };
     });
   }
@@ -36,7 +50,9 @@ class IndecisionApp extends React.Component {
           options={this.state.options}
           handleDeleteOptions={this.handleDeleteOptions}
         />
-        <AddOption />
+        <AddOption 
+          handleAddOption = {this.handleAddOption}
+        />
       </div>
     );
   }
@@ -94,20 +110,31 @@ class Option extends React.Component {
 
 // AddOption
 class AddOption extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.state = {
+      error: undefined
+    }
+  }
   handleAddOption(e) {
     e.preventDefault();
 
     const option = e.target.elements.option.value.trim();
-    if(option) {
-      alert(option);
-    }
+    const error = this.props.handleAddOption(option);
+    this.setState(() => {
+      return { error };
+    });
   }
   render() {
     return (
+      <div>
+      {this.state.error && <p>{this.state.error}</p>}
       <form onSubmit={this.handleAddOption}>
         <input name="option" type="text"/>
         <button type="submit">Add Option</button>
       </form>
+      </div>
     );
   }
 }
