527ea6f022c93a05481ec25a660854b40f156c56
Rick Pearce <rpearce63@yahoo.com>
2018-05-20 12:13:43 -0500

    set up webpack to use babel and run

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log('app.js is running!');
+// install -> import -> use
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = <p>This is JSX from Webpack</p>
+
+ReactDOM.render(template, document.getElementById('app'));
