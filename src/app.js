96fb03e99d9b16b2f37180ef43b858c10bed766b
Rick Pearce <rpearce63@yahoo.com>
2018-05-20 16:50:24 -0500

    break app into components

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
-// install -> import -> use
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const template = <p>This is JSX from Webpack</p>
-
-ReactDOM.render(template, document.getElementById('app'));
+import IndecisionApp from './components/IndecisionApp';
+  
+  ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+  
