76ba91704aa24796268cb62604ee26260cc17b81
Rick Pearce <rick.pearce.crzs@statefarm.com>
2018-05-21 16:15:44 -0500

    add scss styles

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -72,6 +72,7 @@ export default class IndecisionApp extends React.Component {
     return (
       <div>
         <Header subtitle={subtitle}/>
+        <div className="container">
         <Action 
           hasOptions={this.state.options.length > 0}
           handlePick={this.handlePick}
@@ -84,6 +85,7 @@ export default class IndecisionApp extends React.Component {
         <AddOption 
           handleAddOption = {this.handleAddOption}
         />
+        </div>
         <OptionModal 
           selectedOption={this.state.selectedOption}
           handleCloseModal={this.handleCloseModal}
